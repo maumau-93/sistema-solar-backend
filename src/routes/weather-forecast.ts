f2275bcf48050d98d50ded1e2636d1c49cbde442
maugiraldo93 <mauricio.giraldoacosta+git@gmail.com>
2020-08-08 17:46:49 -0500

    Adding routes for APIs and validators

--- src/routes/weather-forecast.ts
+++ src/routes/weather-forecast.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
+import daySchemaValidator from '../validators/weather-forecast';
+import {
+  getQuantityOfDroughtPeriods,
+  getWeatherForecastByDayNumber,
+  getQuantityOfRainyPeriods,
+  getQuantityOfGoodConditionPeriods
+} from '../api/weather-forecast';
 
 const route = Router();
 
 const weatherRoutes: (app: Router) => void = (app: Router) => {
-  app.use('', route);
-  route.get('/weather/:day');
+  app.use('/weather', route);
+  route.get('/:day', daySchemaValidator, getWeatherForecastByDayNumber);
+  route.get('/drought', getQuantityOfDroughtPeriods);
+  route.get('/rainy', getQuantityOfRainyPeriods);
+  route.get('/optimal', getQuantityOfGoodConditionPeriods);
 };
 
 export default weatherRoutes;
